15d41deecb0e1085a6a1a53b0bd0b68ba6ca87c3
RimaSem <rima.semetaite@gmail.com>
2023-01-23 15:11:54 +0200

    Cleaning up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-import Intro from "./components/Intro";
-import Main from "./components/Main";
+import yellowBlob from "./assets/main-yellow.svg";
+import blueBlob from "./assets/main-blue.svg";
+import QuestionList from "./components/QuestionList";
 
 export default function App() {
+  const [introPage, setIntroPage] = React.useState(true);
+
   return (
     <div className="App">
-      <Main />
+      {introPage ? (
+        <div className="intro-page">
+          <img className="upper-blob" src={yellowBlob} />
+          <h2 className="intro-title">Quizzical</h2>
+          <p className="intro-text">
+            Take a quiz to test your knowledge on a variety of fun and
+            interesting topics!
+          </p>
+          <button
+            className="start-btn"
+            type="button"
+            onClick={() => setIntroPage(false)}
+          >
+            Start quiz
+          </button>
+          <img className="lower-blob" src={blueBlob} />
+        </div>
+      ) : (
+        <QuestionList />
+      )}
     </div>
   );
 }
